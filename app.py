e51fe4dca48aae3cefb1fe8623d9dbf47e596ed3
Seyong Kang <nettie0917@gmail.com>
2021-04-18 18:13:10 +0900

    fix

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ class UserManager():
     def kafka_listener(self, data):
     #   print("ordermanager:", data.value.decode("utf-8"))
         json_data = json.loads(data.value.decode("utf-8"))
-        url= 'http://user.flask-restapi/user/' + str(json_data['customer_id'])       
+        url= 'http://flask-restapi.user/user/' + str(json_data['customer_id'])       
         r = requests.get( url )
         
         if r.status_code != 200:
